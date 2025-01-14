5e2c4c8bd6f28195c233ed59d0fb0962dc9f78f6
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2025-01-13 17:54:33 -0700

    finished MERN stack server.js using authMiddleware

--- server/server.js
+++ server/server.js
@@ -2,24 +2,42 @@ const express = require("express");
 const { ApolloServer } = require("@apollo/server");
 const { expressMiddleware } = require("@apollo/server/express4");
 const path = require("path");
-const { authMiddleware } = require('.utils/auth');
+const { authMiddleware } = require(".utils/auth");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+  typeDefs,
+  resolvers,
 });
 
 const startApolloServer = async () => {
-    await server.start();
+  await server.start();
 
-    app.use(express.urlencoded({ extended: false }));
-}
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
 
-app.listen(PORT, (error) => {
-if (!error)
-    console.log("server is running and app is listening on port" + PORT)
-else
-console.log("error occurred, server cant start", error);
-})
+  app.use(
+    "/graphql",
+    expressMiddleware(server, {
+      context: authMiddleware,
+    })
+  );
+
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/dist")));
+
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+    });
+  }
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+};
+
+startApolloServer();
