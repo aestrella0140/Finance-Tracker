4bdc35ed59cfc05726b98d93adbd6ca4cd164718
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2025-01-14 17:52:12 -0700

    finished auth.js using jwt and GraphqlError

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,5 +5,35 @@ const secret = "imBatMan";
 const expiration = "2hr";
 
 module.exports ={
-    AuthenticationError: new GraphQLError("Couldnt authentiucate user")
-}
+    AuthenticationError: new GraphQLError("Couldnt authentiucate user", {
+        extentions: {
+            code: "UNAUTHENTICATED",
+        },
+    }),
+    authMiddleware: function ({ req }) {
+        let token = req.body.token || req.query.token || req.headers.authorization;
+
+        if (req.headers.authorization) {
+            token = token.split(' ').pop().trim();
+        }
+
+        if(!token) {
+            return req;
+        }
+
+        try {
+            const { data } = jwt.verify(token, secret, { maxAge: expiration });
+        } catch {
+            console.log("invalid token");;
+            const decodedToken = jwt.decode(token, { complete: true });
+            console.log(decodedToken);
+        }
+
+        return req;
+    },
+    signToken: function ({ firstName, email, _id }) {
+        const payload = { firstName, email, _id};
+
+        return jwt.sign({ data: payload}, secret, {expiresIn: expiration });
+    },
+};
